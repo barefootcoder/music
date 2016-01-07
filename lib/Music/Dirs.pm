db45defbfdb7a4135d106309331e72dd03eeb7ce
Buddy Burden <barefootcoder@gmail.com>
2016-01-07 02:56:50 -0800

    adding `$XMAS_DIR` to Music::Dirs
    now `add-to-mix` can work with Christmas songs too

--- lib/Music/Dirs.pm
+++ lib/Music/Dirs.pm
@@ -3,13 +3,14 @@ package Music::Dirs;
 use myperl;
 
 use base 'Exporter';
-our @EXPORT = qw< $MUSICHOME $ALBUM_DIR $MISC_DIR $SINGLES_DIR $TRACKLIST_DIR $DROPBOX_DIR track_dirs >;
+our @EXPORT = qw< $MUSICHOME $ALBUM_DIR $MISC_DIR $SINGLES_DIR $XMAS_DIR $TRACKLIST_DIR $DROPBOX_DIR track_dirs >;
 
 
 const our $MUSICHOME => dir($ENV{'MUSICHOME'});
 const our $ALBUM_DIR => $MUSICHOME->subdir('Albums');
 const our $MISC_DIR => $MUSICHOME->subdir('Misc');
 const our $SINGLES_DIR => $MUSICHOME->subdir('Singles');
+const our $XMAS_DIR => $MUSICHOME->subdir('xmas');
 const our $TRACKLIST_DIR => $MUSICHOME->subdir('tracklists');
 const our $DROPBOX_DIR => dir(<~buddy>, 'Dropbox', 'music');
 
