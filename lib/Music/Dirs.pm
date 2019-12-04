65e07b4089f8dbc0351e3d2438433078d68e9eb5
Buddy Burden <barefootcoder@gmail.com>
2019-12-03 18:21:29 -0800

    new command: `album-in-mixes`
    plus tweaks for Music::Dirs:
    -	make `is_alum_dir` use `album_dirs` (which ... duh)
    -	new function: `find_track`
    --		(although `album-in-mixes` doesn't use `find_track`, weirdly ...)

--- lib/Music/Dirs.pm
+++ lib/Music/Dirs.pm
@@ -19,17 +19,6 @@ const our $XMAS_DIR => $MUSICHOME->subdir('xmas');
 const our $TRACKLIST_DIR => $MUSICHOME->subdir('tracklists');
 
 
-func is_album_dir ($dir)
-{
-	return -d $dir and ($ALBUM_DIR->resolve->contains($dir) or $MISC_DIR->resolve->contains($dir));
-}
-
-func is_tracklist ($file)
-{
-	return -f $file and $file =~ /\.m3u$/ and $TRACKLIST_DIR->resolve->contains($file);
-}
-
-
 func album_dirs ()
 {
 	return ($ALBUM_DIR, $CHRISTY_DIR, $MISC_DIR);
@@ -48,6 +37,36 @@ func track_dirs ()
 func all_albums ()
 {
 	return map { $_->children } album_dirs;
+}
+
+
+func is_album_dir ($dir)
+{
+	return -d $dir and grep { $_->resolve->contains($dir) } album_dirs;
+}
+
+func is_tracklist ($file)
+{
+	return -f $file and $file =~ /\.m3u$/ and $TRACKLIST_DIR->resolve->contains($file);
+}
+
+
+func find_track ($artist, $song)
+{
+	my @potentials;
+	if ( $artist =~ s/\h+\[(.*?)\]// )
+	{
+		my $album = $1;
+		push @potentials, glob("$_/*$album*/*$artist* - *$song*") foreach album_dirs;
+	}
+	else
+	{
+		push @potentials, glob("$_/*$artist*/*$artist* - *$song*") foreach album_dirs;
+		push @potentials, glob("$_/*$artist* - *$song*")           foreach single_dirs;
+	}
+	# hopefully there's only one
+	# but, if there's multiples, the caller will have to figure out what to do about it
+	return @potentials;
 }
 
 
